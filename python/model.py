bb7f6a90db28bcbd0b85acfd5871c8f13fd3f3a4
Frank Hereford <frankhereford@users.noreply.github.com>
2024-03-04 10:39:04 -0600

    Update schema creation in model.py

--- python/model.py
+++ python/model.py
@@ -3,5 +3,14 @@
 from lib.database import *
 
 db = get_db_handle()
-drop_schemata_except(db, ["public", "tiger", "tiger_data", "topology"])
-create_schemata(db, ["cris_facts", "visionzero_facts"])
+drop_schemata_except(db, [])
+create_schemata(
+    db,
+    [
+        "cris_facts",
+        "visionzero_facts",
+        "cris_lookup",
+        "visionzero_lookup",
+    ],
+)
+# create_fact_tables(db)
