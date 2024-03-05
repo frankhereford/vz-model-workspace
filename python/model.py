09efe44d76d471a8d20433b02f18066dbc0e115c
Frank Hereford <github@frankhereford.com>
2024-03-04 18:14:13 -0600

    override black

--- python/model.py
+++ python/model.py
@@ -4,29 +4,18 @@ from lib.database import *
 
 db = get_db_handle()
 
+# fmt: off
 drop_schemata_except(db)
 drop_public_entities(db)
 create_schemata(db)
 create_lookup_tables(db)
-artifically_descync_sequences_from_cris_data(
-    db,
-    [
-        "cris_lookup.road_types_sequence",
-        "cris_lookup.unit_types_sequence",
-    ],
-)
+artifically_descync_sequences_from_cris_data( db, ["cris_lookup.road_types_sequence", "cris_lookup.unit_types_sequence"],)
 populate_lookup_tables(db)
 refresh_materialized_views(db)
 create_fact_tables(db)
 create_cris_facts_functions(db)
 create_cris_facts_triggers(db)
-artifically_descync_sequences_from_cris_data(
-    db,
-    [
-        "cris_facts.crashes_id_seq",
-        "cris_facts.units_id_seq",
-    ],
-)
+artifically_descync_sequences_from_cris_data( db, ["cris_facts.crashes_id_seq", "cris_facts.units_id_seq"],)
 create_lookup_table_substitution_triggers(db)
 populate_fact_tables(db, batch_size=10000)
 create_unifying_fact_views(db)
