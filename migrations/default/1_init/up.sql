f5038bd3ac4cccc83c0b47f17af9537113c94ad9
John Clary <john.clary@austintexas.gov>
2024-03-21 07:19:58 -0400

    fix cascades and move remove triggers

--- migrations/default/1_init/up.sql
+++ migrations/default/1_init/up.sql
@@ -57,7 +57,7 @@ alter table db.crashes_cris add constraint cris_road_type_chk
 check (road_type_id < 9000000);
 
 create table db.crashes_vz (
-    crash_id integer not null references db.crashes_cris (
+    crash_id integer primary key not null references db.crashes_cris (
         crash_id
     ) on delete cascade on update restrict,
     road_type_id integer references db.road_types (id)
@@ -88,7 +88,9 @@ alter table db.units_cris add constraint cris_unit_type_chk
 check (unit_type_id < 9000000);
 
 create table db.units_vz (
-    id integer primary key references db.units_cris (id),
+    id integer primary key references db.units_cris (
+        id
+    ) on delete cascade on update restrict,
     crash_id integer references db.crashes_cris (
         crash_id
     ) on delete cascade on update restrict,
@@ -98,7 +100,8 @@ create table db.units_vz (
 );
 
 create table db.units (
-    id integer primary key references db.units_cris (id),
+    id integer primary key references db.units_cris (id)
+    on delete cascade on update restrict,
     crash_id integer not null references db.crashes (
         crash_id
     ) on delete cascade on update restrict,
@@ -127,7 +130,8 @@ create table db.people_cris (
 create index on db.people_cris (unit_id);
 
 create table db.people_vz (
-    id integer references db.people_cris (id),
+    id integer references db.people_cris (id)
+    on delete cascade on update restrict,
     unit_id integer references db.units_cris (
         id
     ),
@@ -140,7 +144,8 @@ create table db.people_vz (
 create index on db.people_vz (unit_id);
 
 create table db.people (
-    id integer references db.people_cris (id),
+    id integer references db.people_cris (id)
+    on delete cascade on update restrict,
     unit_id integer not null references db.units (
         id
     ),
@@ -152,105 +157,3 @@ create table db.people (
 );
 
 create index on db.people (unit_id);
-
--------------------------------------------
--------- Triggers -------------------------
--------------------------------------------
---
--- sets a person record's unit_id column based on the crash_id and unit_nbr
--- the crash_id column exists on the people_cris table, but not on the
--- people_vz or people tables
---
-create or replace function db.people_cris_set_unit_id()
-returns trigger
-language plpgsql
-as $$
-DECLARE
-   unit_record record;
-BEGIN
-    SELECT INTO unit_record *
-        FROM db.units_cris where crash_id = new.crash_id and unit_nbr = new.unit_nbr;
-    new.unit_id = unit_record.id;
-    RETURN new;
-END;
-$$;
-
-create trigger set_cris_person_unit_id
-before insert on db.people_cris
-for each row
-execute procedure db.people_cris_set_unit_id();
-
---
--- handles when a new cris crash record is inserted by copying
--- to the vz_ and unified tables
---
-create or replace function db.crashes_cris_insert_rows()
-returns trigger
-language plpgsql
-as
-$$
-BEGIN
-    -- insert new unified/truth record
-    INSERT INTO db.crashes (crash_id, road_type_id) values (
-        new.crash_id, new.road_type_id
-    );
-    -- insert new (editable) vz record (only record ID)
-    INSERT INTO db.crashes_vz (crash_id) values (new.crash_id);
-    RETURN NULL;
-END;
-$$;
-
-create or replace trigger insert_new_crashes_cris
-after insert on db.crashes_cris
-for each row
-execute procedure db.crashes_cris_insert_rows();
-
---
--- handles when a new unit crash record is inserted by copying
--- to the vz_ and unified tables
---
-create or replace function db.units_cris_insert_rows()
-returns trigger
-language plpgsql
-as
-$$
-BEGIN
-    -- insert new unified/truth record
-    INSERT INTO db.units (id, crash_id, unit_nbr, unit_type_id) values (
-        new.id, new.crash_id, new.unit_nbr, new.unit_type_id
-    );
-    -- insert new (editable) vz record (only record ID)
-    INSERT INTO db.units_vz (id) values (new.id);
-    RETURN NULL;
-END;
-$$;
-
-create or replace trigger insert_new_units_cris
-after insert on db.units_cris
-for each row
-execute procedure db.units_cris_insert_rows();
-
---
--- handles when a new cris people record is inserted by copying
--- to the vz_ and unified tables
---
-create or replace function db.people_cris_insert_rows()
-returns trigger
-language plpgsql
-as
-$$
-BEGIN
-    -- insert new unified/truth record
-    INSERT INTO db.people (id, unit_id, prsn_nbr, unit_nbr, is_primary) values (
-        new.id, new.unit_id, new.prsn_nbr, new.unit_nbr, new.is_primary
-    );
-    -- insert new (editable) vz record (only record ID)
-    INSERT INTO db.people_vz (id) values (new.id);
-    RETURN NULL;
-END;
-$$;
-
-create or replace trigger insert_new_people_cris
-after insert on db.people_cris
-for each row
-execute procedure db.people_cris_insert_rows();
